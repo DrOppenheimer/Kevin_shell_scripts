65a445ca60712bbfa909341f24426c2079452cea
DrOppenheimer <kevin.p.keegan@gmail.com>
2015-12-01 08:27:17 -0600

    added add_dir_2_path.sh script

---
+++ add_dir_2_path.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# simple script to add any directory to PATH
+my_path=`pwd`
+# add to current path
+PATH=$PATH:$my_path
+# add to .profile to be added to the PATH when new session is started
+sudo echo "PATH=$PATH:$my_path>>~/.profile"
+# print complete message
+echo $my_path "has been added to PATH:"
+echo $PATH
