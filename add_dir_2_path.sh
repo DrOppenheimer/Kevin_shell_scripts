897401cd4daabad1bf60d2361763e76af0b1b695
DrOppenheimer <kevin.p.keegan@gmail.com>
2015-12-01 08:39:45 -0600

    debug add_dir_2_path.sh

--- add_dir_2_path.sh
+++ add_dir_2_path.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # simple script to add any directory to PATH
-my_path=`pwd`
+my_path=`pwd`;
 # add to current path
-export PATH=$PATH:$my_path
+export PATH=$PATH:$my_path;
 # add to .profile to be added to the PATH when new session is started
-sudo echo export PATH=$PATH:$my_path >> ~/.profile
+sudo echo export PATH=$PATH:$my_path >> ~/.profile;
 # print complete message
 echo "Old PATH:"
-echo $PATH
+echo $PATH;
 echo "Adding this to PATH:"
-echo $my_path
+echo $my_path;
 echo "New PATH:"
-echo $PATH
+echo $PATH;
