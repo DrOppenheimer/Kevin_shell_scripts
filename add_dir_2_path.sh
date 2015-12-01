d168d22c8c1f795cd8a738743e93154d44e4a9f3
DrOppenheimer <kevin.p.keegan@gmail.com>
2015-12-01 08:30:35 -0600

    debug add_dir_2_path.sh

--- add_dir_2_path.sh
+++ add_dir_2_path.sh
@@ -4,7 +4,7 @@ my_path=`pwd`
 # add to current path
 PATH=$PATH:$my_path
 # add to .profile to be added to the PATH when new session is started
-sudo echo "PATH=$PATH:$my_path>>~/.profile"
+sudo `PATH=$PATH:$my_path >> ~/.profile`
 # print complete message
-echo $my_path "has been added to PATH:"
-echo $PATH
+echo "Adding this to PATH:"
+echo "$my_path"
