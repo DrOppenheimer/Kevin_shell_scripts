d411fe0e39667739dddfbf1e15376837ebdd1e7d
DrOppenheimer <kevin.p.keegan@gmail.com>
2015-12-16 15:01:49 -0600

    added test_rw.sh

---
+++ test_rw.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# number of times to perform the operation
+NUMREPEATS=5;
+MYLOG="rw_log.txt";
+
+MYBUCKET="test_bucket"
+
+FILE1="ERR_tar.1Gb.gz"
+FILE2="ERR_tar.11Gb.gz"
+FILE3="ERR_tar.59Gb.gz"
+
+DENOM=`echo 2^30 | bc` # i.e. bytes in GB
+
+echo "# test_rw.sh log\t"`date` > $MYLOG
+echo "# File\tsize(Gb)\tTransfer_time\tRepeat\tSTREAM\tParcel(?)" >> $MYLOG 
+
+for i in {1..$NUMREPEATS};
+do
+    # upload file 1 (no parcel)
+    my_size=`ls -ltr $FILE1 | cut -d " " -f 5`
+    my_size_gb=`echo $(($my_size / $DENOM))`
+    START_TIME=$SECONDS
+    s3cmd sync ./$FILE1 s3://$MYBUCKET/
+    ELAPSED_TIME=$(($SECONDS - $START_TIME))
+    
+    #loop to print outputs
+    echo $FILE1"\t"$my_size_gb.{$j}"\t"$ELAPSED_TIME"\t"$i"\t"$j"\tN" >> $MYLOG
+    
+done
+
+
+
+
+
+
+
+    
+# # upload
+
+# s3cmd sync ./ERR_tar.1Gb.gz s3://test_bucket/
+
+
+
+# # sudo apt-get install s3cmd
+
+# # list the buckets
+# s3cmd ls
+
+# # list objects in bucket
+# # s3cmd ls s3://BUCKET
+# s3cmd ls s3://test_bucket
+
+# # download a file
+# # s3cmd get s3://BUCKET/OBJECT LOCAL_FILE
+# s3cmd get s3://Onel_lab/test
+
+# # upload
+# # s3cmd sync LOCAL_DIR s3://BUCKET[/PREFIX] or s3://BUCKET[/PREFIX] LOCAL_DIR
+# time s3cmd sync ./ERR_tar.1Gb.gz s3://test_bucket/
+
+
+# process1 &
+# process2 &
+# process3 &
+# process4 &
+# wait
+# process5 &
+# process6 &
+# process7 &
+# process8 &
+# wait
+
+# # # From Satish 12-3-15 # INSTALLING AND USING PARCEL
+# # # Install
+# # python setup.py develop
+# # sudo apt-get install python-pip
+# # sudo python setup.py develop
+# # # Setup
+# # sudo vi /etc/hosts  - add 127.0.0.1 parcel.opensciencedatacloud.org
+# # parcel-tcp2udt 192.170.232.76:9000 &
+# # parcel-udt2tcp localhost:9000 &
+# # wget https://parcel.opensciencedatacloud.org:9000/asgc-geuvadis/ERR188021.tar.gz
+# # # so if u see here.. I have  'python setup.py develop' twice.. this is because it failed first and then I had to do a apt-get install python-pip
+
+# START_TIME=$SECONDS
+# dosomething
+# ELAPSED_TIME=$(($SECONDS - $START_TIME))
+
+
+
+
+# NUMSTREAMS=1
+
+# echo "# test_rw.sh log\t"`date` > $MYLOG
+# echo "# File\tsize(Gb)\tTransfer_time\tRepeat\tSTREAM\tParcel(?)" >> $MYLOG 
+
+# for i in {1..$NUMREPEATS};
+# do
+#     for j in {1..$NUMSTREAMS};
+#     do
+	
+# 	# upload file 1 (no parcel)
+# 	export "my_size.${j}=`ls -ltr $FILE1 | cut -d " " -f 5`"
+# 	export "my_size_gb.${j}=`echo $(($my_size / $DENOM))`"
+# 	export "START_TIME.{j}=$SECONDS"
+# 	s3cmd sync ./$FILE1 s3://$MYBUCKET/ &
+# 	export "ELAPSED_TIME.{j}=$(($SECONDS - $START_TIME))"
+	
+#     done
+
+#     wait
+
+#     for j in {1..$NUMSTREAMS};
+#     do
+
+#     #loop to print outputs
+#     echo $FILE1"\t"$my_size_gb.{$j}"\t"$ELAPSED_TIME"\t"$i"\t"$j"\tN" >> $MYLOG
+    
+# done
+
+
+
+
+
+
+
+
+
+# # upload
+# # s3cmd sync
+
+# # three types of for iteration:
+# # for i in 1 2 3 4 5
+# # for i in {1..10}
+# # for i in {0..10..2} # 1 to 10 by 2
