26da51af2baa78d7dbd108ad978e2858de484439
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-01-04 17:21:52 -0600

    debug test_rw_beta.sh

--- test_rw_beta.sh
+++ test_rw_beta.sh
@@ -349,11 +349,14 @@ download_file_boto(){
 	    echo "DONE 0"
 	    ELAPSED_TIME=`echo "$FINISH_TIME - $START_TIME" | bc -l`
 	    echo "DONE 1"
-	    echo "SIZE: "$my_size_gb
-	    echo "ELAPSED TIME"$ELAPSED_TIME
+	    
 	    echo "DONE 2"
 	    my_size_gb=`echo  $my_size / $DENOMGB | bc -l`
 	    my_size_mb=`echo  $my_size / $DENOMMB | bc -l`
+
+	    echo "SIZE: "$my_size_gb
+	    echo "ELAPSED TIME"$ELAPSED_TIME
+
 	    my_transfer_rate_gps=`echo  $my_size_gb / $ELAPSED_TIME | bc -l`
 	    my_transfer_rate_mps=`echo  $my_size_mb / $ELAPSED_TIME | bc -l`
 	    #echo -e $FILE"\t"`date`"\t"$my_size_gb"\t"$OPERATION"\t"$ELAPSED_TIME"\t"$my_transfer_rate_gps"\t"$my_transfer_rate_mps"\t"$i >> $LOG
