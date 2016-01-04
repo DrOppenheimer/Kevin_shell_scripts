0234be05147af2765aa994fb2ea3d80da12642ef
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-01-04 15:08:14 -0600

    debug test_rw_beta.sh

--- test_rw_beta.sh
+++ test_rw_beta.sh
@@ -346,12 +346,12 @@ download_file_boto(){
 	    echo -e "\nRunning: \"boto_dl.py -f $FILE -a $ACCESSKEY -s $SECRETKEY -b $BUCKET -g $GATEWAY\"\n"
 	    boto_dl.py -f $FILE -a $ACCESSKEY -s $SECRETKEY -b $BUCKET -g $GATEWAY
 	    FINISH_TIME=`date +%s.%N`
-	    ELAPSED_TIME=$(echo "$FINISH_TIME - $START_TIME" |bc)
-	    my_transfer_rate=$(echo "$my_size_gb/$ELAPSED_TIME"|bc)
-	    my_size_gb=$(echo "$my_size/$DENOMGB"|bc)
-	    my_size_mb=$(echo "$my_size/$DENOMMB"|bc)
-	    my_transfer_rate_gps=$(echo "$my_size_gb/$ELAPSED_TIME"|bc)
-	    my_transfer_rate_mps=$(echo "$my_size_mb/$ELAPSED_TIME"|bc)
+	    ELAPSED_TIME=$((echo "$FINISH_TIME - $START_TIME" |bc))
+	    my_transfer_rate=$(echo "$my_size_gb/$ELAPSED_TIME"|bc))
+	    my_size_gb=$((echo "$my_size/$DENOMGB"|bc))
+	    my_size_mb=$((echo "$my_size/$DENOMMB"|bc))
+	    my_transfer_rate_gps=$((echo "$my_size_gb/$ELAPSED_TIME"|bc))
+	    my_transfer_rate_mps=$((echo "$my_size_mb/$ELAPSED_TIME"|bc))
 	    #echo -e $FILE"\t"`date`"\t"$my_size_gb"\t"$OPERATION"\t"$ELAPSED_TIME"\t"$my_transfer_rate_gps"\t"$my_transfer_rate_mps"\t"$i >> $LOG
 
 	    echo -e $FILE"\t"`date`"\t"$my_size_gb"\t"$OPERATION"\t"$ELAPSED_TIME"\t"$my_transfer_rate_gps"\t"$my_transfer_rate_mps"\t"$i >> $LOG
