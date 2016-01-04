43ec0a5e97d51f7f114b2c2b23aac2a82fc411d7
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-01-04 17:19:54 -0600

    debug test_rw_beta.sh

--- test_rw_beta.sh
+++ test_rw_beta.sh
@@ -349,8 +349,8 @@ download_file_boto(){
 	    echo "DONE 0"
 	    ELAPSED_TIME=`echo "$FINISH_TIME - $START_TIME" | bc -l`
 	    echo "DONE 1"
-	    echo $my_size_gb
-	    echo $ELAPSED_TIME
+	    echo "SIZE: "$my_size_gb
+	    echo "ELAPSED TIME"$ELAPSED_TIME
 	    echo "DONE 2"
 	    my_size_gb=`echo  $my_size / $DENOMGB | bc -l`
 	    my_size_mb=`echo  $my_size / $DENOMMB | bc -l`
