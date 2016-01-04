1673556e89a6d171a01e09300db9958764c88231
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-01-04 17:14:41 -0600

    debug test_rw_beta.sh

--- test_rw_beta.sh
+++ test_rw_beta.sh
@@ -345,11 +345,11 @@ download_file_boto(){
 	    #s3cmd get s3://$BUCKET/$FILE
 	    echo -e "\nRunning: \"boto_dl.py -f $FILE -a $ACCESSKEY -s $SECRETKEY -b $BUCKET -g $GATEWAY\"\n"
 	    boto_dl.py -f $FILE -a $ACCESSKEY -s $SECRETKEY -b $BUCKET -g $GATEWAY
-	    FINISH_TIME=`date +%s.%N`
+	    #FINISH_TIME=`date +%s.%N`
 	    echo "DONE 0"
 	    ELAPSED_TIME=`echo $FINISH_TIME - $START_TIME | bc -l`
 	    echo "DONE 1"
-	    my_transfer_rate=`echo $my_size_gb / $ELAPSED_TIME | bc -l`
+	    my_transfer_rate=`echo $my_size_gb/$ELAPSED_TIME|bc`  # z=`echo $y/$x|bc`
 	    echo "DONE 2"
 	    my_size_gb=`echo  $my_size / $DENOMGB | bc -l`
 	    my_size_mb=`echo  $my_size / $DENOMMB | bc -l`
