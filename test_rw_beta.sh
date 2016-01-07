45349a59d2c1a482481d129eec4d1794db3cac44
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-01-07 11:04:42 -0600

    update/debug

--- test_rw_beta.sh
+++ test_rw_beta.sh
@@ -592,7 +592,7 @@ upload_file_boto_withp(){
 	    
 	    START_TIME=`date +%s.%N`
 	    #s3cmd get s3://$BUCKET/$FILE
-	    echo -e "REP $i Running: \"boto_ul.py -f $FILE -a $ACCESSKEY -s $SECRETKEY -b $BUCKET -g $GATEWAY -p\"" >> $ERRORLOG
+	    #echo -e "REP $i Running: \"boto_ul.py -f $FILE -a $ACCESSKEY -s $SECRETKEY -b $BUCKET -g $GATEWAY -p\"" >> $ERRORLOG
 	    boto_ul.py -f $FILE -a $ACCESSKEY -s $SECRETKEY -b $BUCKET -g $GATEWAY -p
 	    FINISH_TIME=`date +%s.%N`
 	    ELAPSED_TIME=`echo "$FINISH_TIME - $START_TIME" | bc -l`
