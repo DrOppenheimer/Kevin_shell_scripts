f9d8bad236dca6ce0447ccf0d9f248e1206ce0df
Kevin P Keegan <kevin@Kevins-Macintosh.local>
2016-05-24 14:35:43 -0500

    fix permissions for proxy_toggle.sh

--- proxy_toggle.sh
+++ proxy_toggle.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # simple tool to toggle proxy vars on or off
-if [ -v ${http_proxy} ]; then
-    echo "setting proxy_vars";
-    . ~/git/Kevin_shell_scripts/set_proxy;
-else
+if [ -v $http_proxy ]; then
     echo "unsetting proxy_vars";
     . ~/git/Kevin_shell_scripts/unset_proxy;
+else
+    echo "setting proxy_vars";
+    . ~/git/Kevin_shell_scripts/set_proxy;
 fi
 # http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
 # if [ -z ${var+x} ]; then echo "var is unset"; else echo "var is set to '$var'"; fi
