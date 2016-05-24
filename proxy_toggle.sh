93ecd834d9de5685eea1bb3afa23063dbacec583
Kevin P Keegan <kevin@Kevins-Macintosh.local>
2016-05-24 13:51:08 -0500

    fix permissions for proxy_toggle.sh

--- proxy_toggle.sh
+++ proxy_toggle.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 # simple tool to toggle proxy vars on or off
-
-my_proxy="http://cloud-proxy:3128"
-
 if [ -v ${http_proxy+x} ]; then
     echo "unsetting proxy_vars";
-    unset http_proxy;
-    unset https_proxy;
-    unset ftp_proxy;
+    . ~/git/unset_proxy;
 else
     echo "setting proxy_vars";
-    export http_proxy=$my_proxy;
-    export https_proxy=$my_proxy;
-    export ftp_proxy=$my_proxy;
+    . ~/git/set_proxy;
 fi
-
 # http://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
 # if [ -z ${var+x} ]; then echo "var is unset"; else echo "var is set to '$var'"; fi
