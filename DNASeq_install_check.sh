0c3f4c5090ff14aa46319f356c6cf8e2e8f8f23b
Kevin P Keegan <kevin@Kevins-Macintosh.local>
2016-07-28 17:40:00 -0500

    update

--- DNASeq_install_check.sh
+++ DNASeq_install_check.sh
@@ -6,18 +6,19 @@
 # if it detects that the installation has already been completed
 
 INSTALLER="/home/ubuntu/install_DNASeq_pipe.sh"
-#LOG="/home/ubuntu/.DNASeq.install_log.txt"
+LOG="/home/ubuntu/.DNASeq.install_log.txt"
 
 if [ ! -f $INSTALLER ]; then  # THERE IS INSTALLER
     cd /home/ubuntu/
     export https_proxy=https://cloud-proxy:3128
     wget https://raw.githubusercontent.com/DrOppenheimer/Kevin_shell_scripts/master/install_DNASeq_pipe.sh
-    install_DNASeq_pipe.sh
-    #install_DNASeq_pipe.sh 2>&1 | tee $LOG
+    chmod +x install_DNASeq_pipe.sh
+    #./install_DNASeq_pipe.sh
+    ./install_DNASeq_pipe.sh 2>&1 | tee -a $LOG
 else
     cd /home/ubuntu/
-    install_DNASeq_pipe.sh
-    #install_DNASeq_pipe.sh 2>&1 | tee $LOG
+    #./install_DNASeq_pipe.sh
+    ./install_DNASeq_pipe.sh 2>&1 | tee -a $LOG
 fi
 
 
